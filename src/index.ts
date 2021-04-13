f1a8ce086a7c1ab04135c6f9186d5a30f75168ef
Sean Ye <seanye24@gmail.com>
2021-04-13 16:52:44 -0700

    Update server file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('lets fucking go');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App is running on PORT ${PORT}`);
+});
