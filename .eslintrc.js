70c078b99e84e948b9a3760b0d8bd0bc8c122eb5
Sean Ye <seanye24@gmail.com>
2021-04-13 16:52:24 -0700

    Update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
-modules.exports = {
+module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
-    'airbnb-typescript',
+    'airbnb-typescript/base',
     'plugin:node/recommended-module',
     'plugin:eslint-comments/recommended',
     'plugin:prettier/recommended',
   ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
 };
