e05f4196ddb20a3609cf2435c4df836bc4952373
Sean Ye <seanye24@gmail.com>
2021-07-08 20:07:02 -0700

    Refactor tossup query into query service

--- src/controllers/tossups.ts
+++ src/controllers/tossups.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { isNumberArray } from '../utils/typeGuards';
-import db from '../utils/db';
+import { queryTossups } from '../utils/db';
 import { error, info } from '../utils/logger';
 
 const tossupsRouter = require('express').Router();
@@ -11,41 +11,29 @@ tossupsRouter.get('/', async (req: Request, res: Response) => {
   const { difficulties } = queryOptions;
 
   if ('categories' in queryOptions && !isNumberArray(categories)) {
-    return res
+    res
       .status(400)
       .send('The query string field `categories` must be a number array.');
   }
 
   if ('difficulties' in queryOptions && !isNumberArray(difficulties)) {
-    return res
+    res
       .status(400)
       .send('The query string field `difficulties` must be a number array.');
   }
 
-  const categoriesQueryString = categories
-    ? `tossups.category_id in (${categories.join(', ')})`
-    : true;
-  const difficultiesQueryString = difficulties
-    ? `tournaments.difficulty in (${difficulties.join(', ')})`
-    : true;
-
   try {
-    const query = `
-      select tournaments.name as tournament_name, tournaments.difficulty as tournament_difficulty, text, answer, formatted_text, formatted_answer from tossups
-      inner join tournaments on tossups.tournament_id = tournaments.id
-      where ${categoriesQueryString} and ${difficultiesQueryString}
-      order by random()
-      limit 10;
-    `;
-    const data: any = await db.query(query);
-    const tossups = data.rows.map((tu: any) => ({
-      tournament: {
-        name: tu.tournament_name,
-        difficulty: tu.tournament_difficulty,
-      },
-      ...tu,
-    }));
-    res.json(tossups);
+    if (isNumberArray(categories) && isNumberArray(difficulties)) {
+      const data = await queryTossups(categories, difficulties);
+      const tossups = data.rows.map((tu: any) => ({
+        tournament: {
+          name: tu.tournament_name,
+          difficulty: tu.tournament_difficulty,
+        },
+        ...tu,
+      }));
+      res.json(tossups);
+    }
   } catch (e) {
     error(e);
   }
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -8,4 +8,25 @@ const client = new Client({
 });
 client.connect();
 
-export default client;
+export const queryTossups = async (
+  categories: number[],
+  difficulties: number[]
+) => {
+  const categoriesQueryString =
+    categories.length > 0
+      ? `tossups.category_id in (${categories.join(', ')})`
+      : true;
+  const difficultiesQueryString =
+    difficulties.length > 0
+      ? `tournaments.difficulty in (${difficulties.join(', ')})`
+      : true;
+  const query = `
+    select tournaments.name as tournament_name, tournaments.difficulty as tournament_difficulty, text, answer, formatted_text, formatted_answer from tossups
+    inner join tournaments on tossups.tournament_id = tournaments.id
+    where ${categoriesQueryString} and ${difficultiesQueryString}
+    order by random()
+    limit 10;
+  `;
+
+  return client.query(query);
+};
