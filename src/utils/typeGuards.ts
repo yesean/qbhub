f271e2e7dd454b84b7f116627cd42edec75c0a0a
Sean Ye <seanye24@gmail.com>
2021-07-09 18:32:03 -0700

    Update type guards

--- src/utils/typeGuards.ts
+++ src/utils/typeGuards.ts
@@ -1,7 +1,23 @@
-export const isNumeric = (value: string) => {
+export const isNumeric = (value: any) => {
   return !Number.isNaN(value) && !Number.isNaN(parseFloat(value));
 };
 
-export const isNumberArray = (value: any): value is number[] => {
-  return Array.isArray(value) && value.every(isNumeric);
+export const isNumericArray = (value: any[]) => {
+  return value.every(isNumeric);
+};
+
+export const isNumber = (value: any): value is number => {
+  return typeof value === 'number' || value instanceof Number;
+};
+
+export const isNumberArray = (value: any[]): value is number[] => {
+  return value.every(isNumber);
+};
+
+export const isString = (value: any): value is string => {
+  return typeof value === 'string' || value instanceof String;
+};
+
+export const isStringArray = (value: any[]): value is string[] => {
+  return value.every(isString);
 };
