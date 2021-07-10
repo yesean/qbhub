d86e17ea72c9db8190aa38e09ecac4c32847c669
Sean Ye <seanye24@gmail.com>
2021-07-09 18:32:43 -0700

    Update tossup endpoint

--- src/controllers/tossups.ts
+++ src/controllers/tossups.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
-import { isNumberArray } from '../utils/typeGuards';
+import {
+  isNumberArray,
+  isNumericArray,
+  isStringArray,
+} from '../utils/typeGuards';
 import { queryTossups } from '../utils/db';
 import { error, info } from '../utils/logger';
 
@@ -7,33 +11,46 @@ const tossupsRouter = require('express').Router();
 
 tossupsRouter.get('/', async (req: Request, res: Response) => {
   const { query: queryOptions } = req;
-  const { categories } = queryOptions;
-  const { difficulties } = queryOptions;
+  const { categories = [] } = queryOptions;
+  const { difficulties = [] } = queryOptions;
 
-  if ('categories' in queryOptions && !isNumberArray(categories)) {
-    res
+  if (!Array.isArray(categories) || !isStringArray(categories)) {
+    return res
+      .status(400)
+      .send('The query string field `categories` must be an array.');
+  }
+
+  if (!Array.isArray(difficulties) || !isStringArray(difficulties)) {
+    return res
+      .status(400)
+      .send('The query string field `difficulties` must be an array.');
+  }
+
+  if (!isNumericArray(categories)) {
+    return res
       .status(400)
       .send('The query string field `categories` must be a number array.');
   }
 
-  if ('difficulties' in queryOptions && !isNumberArray(difficulties)) {
-    res
+  if (!isNumericArray(difficulties)) {
+    return res
       .status(400)
       .send('The query string field `difficulties` must be a number array.');
   }
 
+  const parsedCategories = categories.map((e) => parseInt(e, 10));
+  const parsedDifficulties = difficulties.map((e) => parseInt(e, 10));
+
   try {
-    if (isNumberArray(categories) && isNumberArray(difficulties)) {
-      const data = await queryTossups(categories, difficulties);
-      const tossups = data.rows.map((tu: any) => ({
-        tournament: {
-          name: tu.tournament_name,
-          difficulty: tu.tournament_difficulty,
-        },
-        ...tu,
-      }));
-      res.json(tossups);
-    }
+    const data = await queryTossups(parsedCategories, parsedDifficulties);
+    const tossups = data.rows.map((tu: any) => ({
+      tournament: {
+        name: tu.tournament_name,
+        difficulty: tu.tournament_difficulty,
+      },
+      ...tu,
+    }));
+    res.json(tossups);
   } catch (e) {
     error(e);
   }
