6b503befe225d07646de5e7b473ccdef77612c4a
Sean Ye <seanye24@gmail.com>
2021-08-21 01:52:40 -0700

    Add UserInput

--- src/components/TossupReader/index.tsx
+++ src/components/TossupReader/index.tsx
@@ -3,14 +3,16 @@ import { Flex } from '@chakra-ui/react';
 import Info from './Info';
 import Question from './Question';
 import Answer from './Answer';
+import UserInput from './UserInput';
 import Score from './Score';
 
 const TossupReader: React.FC = () => {
   return (
     <Flex direction="column">
       <Info />
-      <Question />
       <Answer />
+      <Question />
+      <UserInput />
       <Score />
     </Flex>
   );
