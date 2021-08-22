452fe5d9d3c664f1e5685caa6ddc338cb366cf8a
Sean Ye <seanye24@gmail.com>
2021-08-21 18:57:03 -0700

    Update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'prettier',
   ],
   rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
     'arrow-body-style': 'off',
     'import/prefer-default-export': 'off',
     'react/react-in-jsx-scope': 'off',
