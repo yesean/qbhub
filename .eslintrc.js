13f7d26fca3cc3452565d1bf90976998e96099bf
Sean Ye <seanye24@gmail.com>
2021-08-28 00:06:10 -0700

    Update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   ],
   rules: {
     'react/no-array-index-key': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'arrow-body-style': 'off',
     'import/prefer-default-export': 'off',
     'react/react-in-jsx-scope': 'off',
