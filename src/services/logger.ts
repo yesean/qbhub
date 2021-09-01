6bfa72656c24437a134153b8e95088794f3cbb53
Sean Ye <seanye24@gmail.com>
2021-08-31 21:43:05 -0700

    Update logger

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 
-const shouldLog = false;
+const shouldLog = process.env.NODE_ENV === 'development';
 const nullLogger = (..._: any[]) => {};
 export default {
   info: shouldLog ? console.log : nullLogger,
