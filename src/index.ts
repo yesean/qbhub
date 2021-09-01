f027679cb6ee2d2ec21615c05bce0c4e082a0968
Sean Ye <seanye24@gmail.com>
2021-08-31 20:47:12 -0700

    Add static React build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import cors from 'cors';
+import path from 'path';
 import express from 'express';
 
 import tossupsRouter from './controllers/tossups';
@@ -6,6 +7,11 @@ import tossupsRouter from './controllers/tossups';
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use('/api/tossups', tossupsRouter);
 
