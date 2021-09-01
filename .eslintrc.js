fa0589602fa14f028870fe966d0490b99d177517
Sean Ye <seanye24@gmail.com>
2021-08-31 18:26:56 -0700

    Update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,5 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'airbnb-typescript/base',
-    'plugin:node/recommended-module',
-    'plugin:eslint-comments/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
   parserOptions: {
     project: './tsconfig.json',
   },
