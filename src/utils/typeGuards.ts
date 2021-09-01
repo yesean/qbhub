484ad980bcfa99b2eba79c9c6a33b1b63b534884
Sean Ye <seanye24@gmail.com>
2021-08-31 18:27:10 -0700

    Update formatting

--- src/utils/typeGuards.ts
+++ src/utils/typeGuards.ts
@@ -1,23 +1,16 @@
-export const isNumeric = (value: any) => {
-  return !Number.isNaN(value) && !Number.isNaN(parseFloat(value));
-};
+export const isNumeric = (value: any) =>
+  !Number.isNaN(value) && !Number.isNaN(parseFloat(value));
 
-export const isNumericArray = (value: any[]) => {
-  return value.every(isNumeric);
-};
+export const isNumericArray = (value: any[]) => value.every(isNumeric);
 
-export const isNumber = (value: any): value is number => {
-  return typeof value === 'number' || value instanceof Number;
-};
+export const isNumber = (value: any): value is number =>
+  typeof value === 'number' || value instanceof Number;
 
-export const isNumberArray = (value: any[]): value is number[] => {
-  return value.every(isNumber);
-};
+export const isNumberArray = (value: any[]): value is number[] =>
+  value.every(isNumber);
 
-export const isString = (value: any): value is string => {
-  return typeof value === 'string' || value instanceof String;
-};
+export const isString = (value: any): value is string =>
+  typeof value === 'string' || value instanceof String;
 
-export const isStringArray = (value: any[]): value is string[] => {
-  return value.every(isString);
-};
+export const isStringArray = (value: any[]): value is string[] =>
+  value.every(isString);
