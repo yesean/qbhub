84240e58b9328c4d9a4a96ae0a0fff561344a5af
Sean Ye <seanye24@gmail.com>
2021-08-31 18:27:35 -0700

    Add subcategory support

--- src/controllers/tossups.ts
+++ src/controllers/tossups.ts
@@ -1,23 +1,27 @@
 import { Request, Response } from 'express';
-import {
-  isNumberArray,
-  isNumericArray,
-  isStringArray,
-} from '../utils/typeGuards';
+import { isNumeric, isNumericArray, isStringArray } from '../utils/typeGuards';
 import { queryTossups } from '../utils/db';
-import { error, info } from '../utils/logger';
+import { error } from '../utils/logger';
 
 const tossupsRouter = require('express').Router();
 
 tossupsRouter.get('/', async (req: Request, res: Response) => {
   const { query: queryOptions } = req;
   const { categories = [] } = queryOptions;
+  const { subcategories = [] } = queryOptions;
   const { difficulties = [] } = queryOptions;
+  const { limit = 10 } = queryOptions;
 
   if (!Array.isArray(categories) || !isStringArray(categories)) {
     return res
       .status(400)
       .send('The query string field `categories` must be an array.');
+  }
+
+  if (!Array.isArray(subcategories) || !isStringArray(subcategories)) {
+    return res
+      .status(400)
+      .send('The query string field `subcategories` must be an array.');
   }
 
   if (!Array.isArray(difficulties) || !isStringArray(difficulties)) {
@@ -32,28 +36,44 @@ tossupsRouter.get('/', async (req: Request, res: Response) => {
       .send('The query string field `categories` must be a number array.');
   }
 
+  if (!isNumericArray(subcategories)) {
+    return res
+      .status(400)
+      .send('The query string field `subcategories` must be a number array.');
+  }
+
   if (!isNumericArray(difficulties)) {
     return res
       .status(400)
       .send('The query string field `difficulties` must be a number array.');
   }
 
+  if (!isNumeric(limit)) {
+    return res
+      .status(400)
+      .send('The query string field `limit` must be a number.');
+  }
+
   const parsedCategories = categories.map((e) => parseInt(e, 10));
+  const parsedSubcategories = subcategories.map((e) => parseInt(e, 10));
   const parsedDifficulties = difficulties.map((e) => parseInt(e, 10));
+  const parsedLimit = Number(limit);
 
   try {
-    const data = await queryTossups(parsedCategories, parsedDifficulties);
+    const data = await queryTossups(
+      parsedCategories,
+      parsedSubcategories,
+      parsedDifficulties,
+      parsedLimit
+    );
     const tossups = data.rows.map((tu: any) => ({
-      tournament: {
-        name: tu.tournament_name,
-        difficulty: tu.tournament_difficulty,
-      },
       ...tu,
     }));
     res.json(tossups);
   } catch (e) {
     error(e);
   }
+  return () => {};
 });
 
 export default tossupsRouter;
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,4 +1,5 @@
 import { Client } from 'pg';
+import { Category, Difficulty, Subcategory } from './types';
 
 const client = new Client({
   user: 'sean',
@@ -9,24 +10,52 @@ const client = new Client({
 client.connect();
 
 export const queryTossups = async (
-  categories: number[],
-  difficulties: number[]
+  categories: Category[],
+  subcategories: Subcategory[],
+  difficulties: Difficulty[],
+  limit: number
 ) => {
   const categoriesQueryString =
     categories.length > 0
       ? `tossups.category_id in (${categories.join(', ')})`
+      : false;
+  const subcategoriesQueryString =
+    subcategories.length > 0
+      ? `tossups.subcategory_id in (${subcategories.join(', ')})`
       : true;
+  let combinedCategoryiesQueryString;
+  if (categories.length === 0 && subcategories.length === 0)
+    combinedCategoryiesQueryString = true;
+  else if (subcategories.length > 0)
+    combinedCategoryiesQueryString = subcategoriesQueryString;
+  else if (categories.length > 0)
+    combinedCategoryiesQueryString = categoriesQueryString;
+
   const difficultiesQueryString =
     difficulties.length > 0
       ? `tournaments.difficulty in (${difficulties.join(', ')})`
       : true;
 
+  const columns = [
+    { name: 'text', alias: 'text' },
+    { name: 'answer', alias: 'answer' },
+    { name: 'formatted_text', alias: 'formatted_text' },
+    { name: 'formatted_answer', alias: 'formatted_answer' },
+    { name: 'category_id', alias: 'category' },
+    { name: 'subcategory_id', alias: 'subcategory' },
+    { name: 'difficulty', alias: 'difficulty' },
+    { name: 'name', alias: 'tournament' },
+  ];
+  const columnsCommand = columns
+    .map((c) => `${c.name} as ${c.alias}`)
+    .join(',');
+
   const query = `
-    select tournaments.name as tournament_name, tournaments.difficulty as tournament_difficulty, text, answer, formatted_text, formatted_answer from tossups
+    select ${columnsCommand} from tossups
     inner join tournaments on tossups.tournament_id = tournaments.id
-    where ${categoriesQueryString} and ${difficultiesQueryString}
+    where (${combinedCategoryiesQueryString}) and ${difficultiesQueryString}
     order by random()
-    limit 10;
+    limit ${limit};
   `;
 
   return client.query(query);
