d112ff2584f757e4dc69043b560577c834936eef
Sean Ye <seanye24@gmail.com>
2021-09-04 01:51:53 -0700

    Add keyboard shortcuts modal

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,25 +1,44 @@
-import { Flex, Icon, IconButton, Link, Spacer, Text } from '@chakra-ui/react';
+import { InfoOutlineIcon } from '@chakra-ui/icons';
+import { Flex, Icon, IconButton, Link, Text } from '@chakra-ui/react';
+import { useState } from 'react';
 import { AiFillGithub } from 'react-icons/ai';
+import KeyboardShortcutsModal from '../KeyboardShortcutsModal';
 
 const Footer: React.FC = () => {
+  const [isKeyboardShortcutsModalOpen, setIsKeyboardShortcutsModalOpen] =
+    useState(false);
+
   return (
-    <Flex align="center" justify="center" mt={1} mb={1}>
-      <Text textAlign="center" fontWeight="bold" mr={4}>
-        Created by{' '}
-        <Link href="https://seancye.com" color="cyan.600" isExternal>
-          Sean Ye
-        </Link>
-      </Text>
-      <IconButton
-        aria-label="github link"
-        bg="white"
-        icon={
-          <Link href="https://github.com/seanye24/qbhub-frontend" isExternal>
-            <Icon w={6} h={6} as={AiFillGithub} />{' '}
+    <>
+      <Flex align="center" justify="center" mt={1} mb={1}>
+        <Text textAlign="center" fontWeight="bold" mr={4}>
+          Created by{' '}
+          <Link href="https://seancye.com" color="cyan.600" isExternal>
+            Sean Ye
           </Link>
-        }
+        </Text>
+        <IconButton
+          aria-label="github link"
+          bg="white"
+          icon={
+            <Link href="https://github.com/seanye24/qbhub-frontend" isExternal>
+              <Icon w={6} h={6} as={AiFillGithub} />{' '}
+            </Link>
+          }
+        />
+        <IconButton
+          position="absolute"
+          right={4}
+          aria-label="keyboard shortcuts"
+          onClick={() => setIsKeyboardShortcutsModalOpen(true)}
+          icon={<InfoOutlineIcon />}
+        />
+      </Flex>
+      <KeyboardShortcutsModal
+        isOpen={isKeyboardShortcutsModalOpen}
+        onClose={() => setIsKeyboardShortcutsModalOpen(false)}
       />
-    </Flex>
+    </>
   );
 };
 
---
+++ src/components/KeyboardShortcutsModal/index.tsx
@@ -0,0 +1,75 @@
+import {
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  Kbd,
+  Text,
+} from '@chakra-ui/react';
+
+type KeyboardShortcutsModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const KeyboardShortcutsModal: React.FC<KeyboardShortcutsModalProps> = ({
+  isOpen,
+  onClose,
+}) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      size="6xl"
+      scrollBehavior="inside"
+      isCentered
+    >
+      <ModalOverlay />
+      <ModalContent m={4} maxH="max(75vh, 600px)">
+        <ModalHeader>Keyboard Shortcuts</ModalHeader>
+        <ModalBody pt={0} display="flex" flexDirection="column">
+          <Table variant="simple" mb={4}>
+            <Thead>
+              <Tr>
+                <Th>Key</Th>
+                <Th>Action</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>
+                  <Kbd>n</Kbd>
+                </Td>
+                <Td>
+                  <Text> start reading / next question</Text>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>
+                  <Kbd>space</Kbd>
+                </Td>
+                <Td>buzz in to answer</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="cyan" color="gray.50" mr={3} onClick={onClose}>
+            Done
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default KeyboardShortcutsModal;
