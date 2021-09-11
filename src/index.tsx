112be91d2fa4f3222d736a100e7f867d77135b0c
Sean Ye <seanye24@gmail.com>
2021-09-10 22:24:50 -0700

    Add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root'),
