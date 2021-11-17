0499195b9d78ccc701d706098df76987118cb2be
Sean Ye <seanye24@gmail.com>
2021-11-16 20:51:28 -0800

    Update express static

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,17 @@ import { info } from './utils/logger';
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/tossups', tossupsRouter);
 app.use('/api/freq', freqRouter);
 app.use('/api/clues', cluesRouter);
-app.use('*', express.static(path.join(__dirname, 'build')));
+app.get('/', (_, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.get('*', (_, res) => res.redirect('/'));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  info(`App is running on PORT ${PORT}`);
+  info(`App is running on PORT ${PORT}.`);
 });
