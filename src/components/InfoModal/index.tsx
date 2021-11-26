61be8bc90b0bde6f5ae179ce79569db4a7db0371
Sean Ye <seanye24@gmail.com>
2021-11-26 03:19:24 -0800

    Refactor shortcuts display

--- src/components/InfoModal/index.tsx
+++ src/components/InfoModal/index.tsx
@@ -23,7 +23,7 @@ import TealButton from '../TealButton';
 import { close, selectInfoModal } from './infoModalSlice';
 
 type Shortcut = {
-  label: string;
+  label: string | string[];
   description: string | JSX.Element;
 };
 
@@ -72,13 +72,13 @@ const globalShortcuts = [
 
 const readerShortcuts = [
   { label: 'n', description: 'Start reading / Next question' },
-  { label: 'space', description: 'Buzz' },
+  { label: '⎵', description: 'Buzz' },
   { label: 'h', description: 'Open question history' },
 ];
 
 const freqShortcuts = [
-  { label: 'p/←', description: 'Previous page' },
-  { label: 'n/→', description: 'Next page' },
+  { label: ['p', '←'], description: 'Previous page' },
+  { label: ['n', '→'], description: 'Next page' },
 ];
 
 const cluesShortcuts = [{ label: '/', description: 'Search for answers' }];
@@ -90,8 +90,18 @@ const InfoModal: React.FC = () => {
 
   const closeModal = () => dispatch(close());
 
+  const renderShortcutLabel = (label: string | string[]) => {
+    if (Array.isArray(label)) {
+      return label.map((l) => <Kbd mr={1}>{l}</Kbd>);
+    }
+    return <Kbd>{label}</Kbd>;
+  };
   const renderTable = (shortcuts: Shortcut[]) => (
-    <Table variant="simple" mb={4}>
+    <Table variant="simple" mb={4} style={{ tableLayout: 'fixed' }}>
+      <colgroup>
+        <col style={{ width: '25%' }} />
+        <col style={{ width: '75%' }} />
+      </colgroup>
       <Thead>
         <Tr>
           <Th>Key</Th>
@@ -101,9 +111,7 @@ const InfoModal: React.FC = () => {
       <Tbody>
         {shortcuts.map(({ label, description }) => (
           <Tr>
-            <Td>
-              <Kbd>{label}</Kbd>
-            </Td>
+            <Td>{renderShortcutLabel(label)}</Td>
             <Td>
               <Text>{description}</Text>
             </Td>
@@ -117,7 +125,7 @@ const InfoModal: React.FC = () => {
     if (pathname.startsWith(ROUTES.reader.root)) {
       return (
         <>
-          <Heading size="sm">Reader</Heading>
+          <Heading size="sm">Reader Shortcuts</Heading>
           {renderTable(readerShortcuts)}
         </>
       );
@@ -125,7 +133,7 @@ const InfoModal: React.FC = () => {
     if (pathname.startsWith(ROUTES.freq.root)) {
       return (
         <>
-          <Heading size="sm">Frequency List</Heading>
+          <Heading size="sm">Frequency List Shortcuts</Heading>
           {renderTable(freqShortcuts)}
         </>
       );
@@ -133,7 +141,7 @@ const InfoModal: React.FC = () => {
     if (pathname.startsWith(ROUTES.clues.root)) {
       return (
         <>
-          <Heading size="sm">Clues Generator</Heading>
+          <Heading size="sm">Clues Generator Shortcuts</Heading>
           {renderTable(cluesShortcuts)}
         </>
       );
@@ -150,12 +158,12 @@ const InfoModal: React.FC = () => {
       isCentered
     >
       <ModalOverlay />
-      <ModalContent m={4} maxH="max(75vh, 600px)">
+      <ModalContent m={4} maxW="600px" maxH="max(75vh, 600px)">
         <ModalHeader color="black">Info</ModalHeader>
         <ModalBody pt={0} display="flex" flexDirection="column">
           {renderLocalShortcuts()}
           <Heading size="sm" color="gray.800">
-            Global
+            Global Shortcuts
           </Heading>
           {renderTable(globalShortcuts)}
         </ModalBody>
