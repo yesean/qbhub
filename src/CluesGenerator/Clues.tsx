f771528d62e8417c496701528f8cb8722395d49b
Sean Ye <seanye24@gmail.com>
2021-11-29 02:00:30 -0800

    Add tooltip to clue

--- src/CluesGenerator/Clues.tsx
+++ src/CluesGenerator/Clues.tsx
@@ -1,8 +1,9 @@
-import { CircularProgress } from '@chakra-ui/react';
+import { CircularProgress, Text, Tooltip } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { TwoColumnTable } from '../components/Table';
+import { Clue } from '../types/tossups';
 import {
   CluesGeneratorStatus,
   fetchClues,
@@ -23,13 +24,45 @@ const Clues: React.FC = () => {
     dispatch(fetchClues(answer));
   }, [dispatch, answer]);
 
+  const renderTooltip = (clue: Clue) => {
+    const startIndex = clue.sentence.indexOf(clue.clue);
+    if (startIndex === -1) {
+      return <Text>{clue.sentence}</Text>;
+    }
+    return (
+      <>
+        <Text display="inline">{clue.sentence.substring(0, startIndex)}</Text>
+        <Text display="inline" as="mark" bgColor="#fffea9">
+          {clue.sentence.substring(startIndex, startIndex + clue.clue.length)}
+        </Text>
+        <Text display="inline">
+          {clue.sentence.substring(startIndex + clue.clue.length)}
+        </Text>
+      </>
+    );
+  };
+
+  const renderClue = (clue: Clue) => {
+    return (
+      <Tooltip
+        hasArrow
+        placement="top"
+        label={renderTooltip(clue)}
+        borderRadius="12px"
+        p={2}
+      >
+        <Text>{clue.clue}</Text>
+      </Tooltip>
+    );
+  };
+
   const render = () => {
     if (status !== CluesGeneratorStatus.idle) {
       return <CircularProgress isIndeterminate color="cyan" />;
     }
     return (
       <TwoColumnTable
-        data={clues}
+        data={clues.map((clue) => ({ ...clue, clue: renderClue(clue) }))}
         headers={cluesFields}
         width={600}
         height={700}
