524a31c5468af8c9f864949f851d4468ffab53a7
Sean Ye <seanye24@gmail.com>
2021-11-29 23:49:13 -0800

    Add docs

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,5 +1,11 @@
+/**
+ * Return a random number in a range, given the start and length of the range.
+ */
 export const random = (start = 0, len = 1) =>
   Math.floor(Math.random() * len + start);
 
+/**
+ * Round a number to a specified number of decimal places.
+ */
 export const round = (n: number, decimalPlaces: number = 0) =>
   Math.round(n * 10 ** decimalPlaces) / 10 ** decimalPlaces;
