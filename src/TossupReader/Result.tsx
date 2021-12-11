82a551684206a41c4b1498ac0dbe7532b4d2d24c
Sean Ye <seanye24@gmail.com>
2021-12-11 04:23:23 -0800

    Add text for prompting

--- src/TossupReader/Result.tsx
+++ src/TossupReader/Result.tsx
@@ -1,15 +1,16 @@
 import { Heading } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 import { TossupScore } from '../types/tossups';
-import { selectTossupReader } from './tossupReaderSlice';
+import { ReaderStatus, selectTossupReader } from './tossupReaderSlice';
 
 const Result: React.FC = () => {
-  const { currentResult: result } = useSelector(selectTossupReader);
+  const { currentResult: result, status } = useSelector(selectTossupReader);
 
   let text;
-  if (result.score === TossupScore.power) text = '🎉  Power!  🥳';
-  else if (result.score === TossupScore.ten) text = '😇  Ten!';
-  else if (result.score === TossupScore.neg) text = '😩  Neg!';
+  if (status === ReaderStatus.prompting) text = 'Prompt!';
+  else if (result.score === TossupScore.power) text = '🎉  Power!  🥳';
+  else if (result.score === TossupScore.ten) text = 'Ten!';
+  else if (result.score === TossupScore.neg) text = 'Neg!';
 
   return (
     <Heading pl={4} textAlign="center" size="md" whiteSpace="pre" mb={4}>
