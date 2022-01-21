53f7022f7cb21b9d7e44f1554830d5a5248b813d
Sean Ye <seanye24@gmail.com>
2022-01-21 12:51:33 -0800

    Update fonts and theme

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,17 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { store } from './app/store';
+import theme from './theme';
+
+import '@fontsource/raleway/700.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Raleway',
+    body: 'Inter',
+  },
+});
+
+export default theme;
