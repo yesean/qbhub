e99396177c14a6c330a88c9da880be59bd13bcc0
Sean Ye <seanye24@gmail.com>
2022-03-22 18:37:18 -0700

    virtualize tossup history modal

---
+++ src/components/TossupHistoryModal/TossupResults.tsx
@@ -0,0 +1,124 @@
+import { Box, Center, Flex, Heading } from '@chakra-ui/react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import { VariableSizeList } from 'react-window';
+import { TossupResult } from '../../types/tossups';
+import { renderQuestion } from '../../utils/reader';
+import { parseHTMLString } from '../../utils/string';
+
+type TossupRowProps = {
+  style: React.CSSProperties;
+  index: number;
+  data: {
+    results: TossupResult[];
+    setRowHeight: (index: number, size: number) => void;
+  };
+};
+
+const TossupRow: React.FC<TossupRowProps> = ({
+  style,
+  index,
+  data: { results, setRowHeight },
+}) => {
+  const rowRef = useRef<HTMLTableRowElement>(null);
+  const boxRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (rowRef.current) {
+      let height = rowRef.current.scrollHeight;
+      if (boxRef.current)
+        height = Math.max(height, boxRef.current.scrollHeight + 20);
+      setRowHeight(index, height);
+    }
+  }, [index, rowRef, boxRef, setRowHeight]);
+
+  const result = results[index];
+
+  return (
+    <Flex
+      style={style}
+      overflowX="auto"
+      ref={rowRef}
+      backgroundColor={result.score > 0 ? 'green.200' : 'red.200'}
+      key={index}
+      outline="1px solid #e2e8f0"
+      py="10px"
+    >
+      <Center flex="1 0">{result.score}</Center>
+      <Center flex="1 0">{result.userAnswer || '<no answer>'}</Center>
+      <Center flex="2 0">
+        <Box width="75%" textAlign="center">
+          {parseHTMLString(result.tossup.formattedAnswer)}
+        </Box>
+      </Center>
+      <Box flex="4 0" overflow="auto" h={boxRef.current?.scrollHeight}>
+        <Box minW={80} ref={boxRef}>
+          {renderQuestion(result.words, {
+            buzz: result.buzzIndex,
+          })}
+        </Box>
+      </Box>
+      <Center flex="1.5 0">
+        <Box w="75%">{result.tossup.tournament}</Box>
+      </Center>
+    </Flex>
+  );
+};
+
+const tossupHeaders = [
+  ['Score', 1],
+  ['Input', 1],
+  ['Answer', 2],
+  ['Question', 4],
+  ['Tournament', 1.5],
+];
+
+type TossupResultProps = {
+  results: TossupResult[];
+};
+
+const TossupResults: React.FC<TossupResultProps> = ({ results }) => {
+  const listRef = useRef<VariableSizeList>(null);
+  const innerRef = useRef<HTMLDivElement>(null);
+  const [rowHeights, setRowHeights] = useState<number[]>([]);
+
+  const setRowHeight = useCallback((index: number, size: number) => {
+    listRef.current?.resetAfterIndex(0);
+    setRowHeights((heights) => ({ ...heights, [index]: size }));
+  }, []);
+
+  const getRowSize = (index: number) => rowHeights[index] ?? 200;
+
+  return (
+    <>
+      <Flex w={innerRef.current?.clientWidth} mb={2}>
+        {tossupHeaders.map(([header, flex], i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <Heading size="sm" key={i} flex={flex} textAlign="center">
+            {header}
+          </Heading>
+        ))}
+      </Flex>
+      <Box flex="1" ref={innerRef}>
+        <AutoSizer>
+          {({ width, height }) => (
+            <VariableSizeList
+              width={width}
+              height={height}
+              estimatedItemSize={400}
+              itemCount={results.length}
+              itemSize={getRowSize}
+              ref={listRef}
+              innerRef={innerRef}
+              itemData={{ results, setRowHeight }}
+            >
+              {TossupRow}
+            </VariableSizeList>
+          )}
+        </AutoSizer>
+      </Box>
+    </>
+  );
+};
+
+export default TossupResults;
--- src/components/TossupHistoryModal/index.tsx
+++ src/components/TossupHistoryModal/index.tsx
@@ -16,22 +16,25 @@ import { useSelector } from 'react-redux';
 import { useAppDispatch } from '../../app/hooks';
 import { selectTossupReader } from '../../TossupReader/tossupReaderSlice';
 import { TossupScore } from '../../types/tossups';
-import { renderQuestion } from '../../utils/reader';
-import { parseHTMLString } from '../../utils/string';
 import TealButton from '../TealButton';
 import { close, selectTossupHistoryModal } from './tossupHistoryModalSlice';
+import TossupResults from './TossupResults';
+
+const scoreHeaders = ['15', '10', '-5', 'Points'];
 
 const TossupHistoryModal: React.FC = () => {
   const dispatch = useAppDispatch();
-
   const { results } = useSelector(selectTossupReader);
   const { isOpen } = useSelector(selectTossupHistoryModal);
+
   const closeModal = () => dispatch(close());
 
   const powers = results.filter((r) => r.score === TossupScore.power).length;
   const tens = results.filter((r) => r.score === TossupScore.ten).length;
   const negs = results.filter((r) => r.score === TossupScore.neg).length;
   const points = results.reduce((acc, r) => acc + r.score, 0);
+
+  const scoreBody = [powers, tens, negs, points];
 
   return (
     <Modal
@@ -42,61 +45,26 @@ const TossupHistoryModal: React.FC = () => {
       isCentered
     >
       <ModalOverlay />
-      <ModalContent m={4} maxW="min(90vw, 1400px)" maxH="max(75vh, 600px)">
+      <ModalContent m={4} w="min(90vw, 1400px)" h="max(75vh, 600px)">
         <ModalHeader>Tossup History</ModalHeader>
         <ModalBody pt={0} display="flex" flexDirection="column">
           <Table variant="simple" mb={4}>
             <Thead>
               <Tr>
-                <Th>15</Th>
-                <Th>10</Th>
-                <Th>-5</Th>
-                <Th>Points</Th>
+                {scoreHeaders.map((header, i) => (
+                  <Th key={i}>{header}</Th>
+                ))}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td>{powers}</Td>
-                <Td>{tens}</Td>
-                <Td>{negs}</Td>
-                <Td>{points}</Td>
+                {scoreBody.map((score, i) => (
+                  <Td key={i}>{score}</Td>
+                ))}
               </Tr>
             </Tbody>
           </Table>
-          <Table
-            variant="simple"
-            display="block"
-            pos="relative"
-            overflow="auto"
-          >
-            <Thead pos="sticky" top={0} bg="white">
-              <Tr>
-                <Th>Score</Th>
-                <Th>Input</Th>
-                <Th>Answer</Th>
-                <Th>Question</Th>
-                <Th>Tournament</Th>
-              </Tr>
-            </Thead>
-            <Tbody zIndex={-1}>
-              {results.map((r) => (
-                <Tr
-                  key={r.tossup.formattedAnswer}
-                  backgroundColor={r.score > 0 ? 'green.200' : 'red.200'}
-                >
-                  <Td>{r.score}</Td>
-                  <Td>{r.userAnswer}</Td>
-                  <Td>{parseHTMLString(r.tossup.formattedAnswer)}</Td>
-                  <Td>
-                    {renderQuestion(r.words, {
-                      buzz: r.buzzIndex,
-                    })}
-                  </Td>
-                  <Td>{r.tossup.tournament}</Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
+          <TossupResults results={results} />
         </ModalBody>
         <ModalFooter>
           <TealButton mr={3} onClick={closeModal}>
