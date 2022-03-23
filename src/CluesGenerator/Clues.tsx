98676bf1b081a4bbfd639d407b875ba3e13fd748
Sean Ye <seanye24@gmail.com>
2022-03-22 18:37:27 -0700

    add download options for clues

--- src/CluesGenerator/Clues.tsx
+++ src/CluesGenerator/Clues.tsx
@@ -1,9 +1,18 @@
-import { CircularProgress, Heading, Text, Tooltip } from '@chakra-ui/react';
-import { useEffect } from 'react';
+import {
+  Box,
+  CircularProgress,
+  Flex,
+  Heading,
+  Text,
+  Tooltip,
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
+import LinkButton from '../components/LinkButton';
 import { TwoColumnTable } from '../components/Table';
 import { Clue } from '../types/tossups';
+import { toCSV, toJSON } from '../utils/array';
 import {
   CluesGeneratorStatus,
   fetchClues,
@@ -19,6 +28,32 @@ const Clues: React.FC = () => {
   const { status, clues } = useAppSelector(selectCluesGenerator);
   const dispatch = useAppDispatch();
   const { answer } = useParams<{ answer: string }>();
+  const [CSVLink, setCSVLink] = useState('');
+  const [JSONLink, setJSONLink] = useState('');
+
+  useEffect(() => {
+    const cluesCSV = clues
+      .filter((clue) => clue.score > 0)
+      .map((clue) => [clue.clue, answer, clue.sentence]);
+    const url = toCSV(cluesCSV);
+    setCSVLink(url);
+
+    return () => {
+      URL.revokeObjectURL(url);
+    };
+  }, [answer, clues]);
+
+  useEffect(() => {
+    const cluesJSON = clues
+      .filter((clue) => clue.score > 0)
+      .map((clue) => ({ ...clue, answer }));
+    const url = toJSON(cluesJSON);
+    setJSONLink(url);
+
+    return () => {
+      URL.revokeObjectURL(url);
+    };
+  }, [answer, clues]);
 
   useEffect(() => {
     dispatch(fetchClues(answer));
@@ -70,13 +105,26 @@ const Clues: React.FC = () => {
     if (status !== CluesGeneratorStatus.idle) {
       return <CircularProgress isIndeterminate color="cyan" />;
     }
+
     return (
-      <TwoColumnTable
-        data={clues.map((clue) => ({ ...clue, clue: renderClue(clue) }))}
-        headers={cluesFields}
-        width={600}
-        height={700}
-      />
+      <>
+        <Box w="min(600px, 100%)" h="min(700px, 100%)" mb={4} overflow="auto">
+          <TwoColumnTable
+            data={clues.map((clue) => ({ ...clue, clue: renderClue(clue) }))}
+            headers={cluesFields}
+            width={600}
+            height={700}
+          />
+        </Box>
+        <Flex justify="center">
+          <LinkButton href={CSVLink} download={answer} mr={4}>
+            Export CSV
+          </LinkButton>
+          <LinkButton href={JSONLink} download={answer}>
+            Export JSON
+          </LinkButton>
+        </Flex>
+      </>
     );
   };
 
