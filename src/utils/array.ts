24e35fb0e8587f16a2a1622cfd7936d0e82f1f23
Sean Ye <seanye24@gmail.com>
2022-03-22 17:57:04 -0700

    add file creation methods

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -32,3 +32,15 @@ export const zip = (...arrs: any[]) => {
     .fill(null)
     .map((_, i) => arrs.map((arr) => arr[i]));
 };
+
+export const toCSV = (rows: any[][]) => {
+  const csvString = rows.map((row) => row.join(',')).join('\n');
+  const blob = new Blob([csvString], { type: 'text/csv' });
+  return URL.createObjectURL(blob);
+};
+
+export const toJSON = (json: any) => {
+  const jsonString = JSON.stringify(json, null, 2);
+  const blob = new Blob([jsonString], { type: 'application/json' });
+  return URL.createObjectURL(blob);
+};
