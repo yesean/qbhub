8a992e2b3236873403d23f0487a67c3f03a20b03
Sean Ye <seanye24@gmail.com>
2022-09-17 09:13:13 -0700

    Change logflare token to env variable

--- src/utils/pino.ts
+++ src/utils/pino.ts
@@ -1,12 +1,9 @@
 import pino from 'pino-http';
 import { createWriteStream } from 'pino-logflare';
 
-// QBHub.digitalocean
-const logflareSourceToken = '8815a6c9-412f-40a7-9abd-88f146cbdc40';
-
 const stream = createWriteStream({
   apiKey: process.env.LOGFLARE_API_KEY,
-  sourceToken: logflareSourceToken,
+  sourceToken: process.env.LOGFLARE_SOURCE_TOKEN,
 });
 
 export default pino({ stream });
