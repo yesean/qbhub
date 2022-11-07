434045cd0713d2a897995ee689c6b2a0a23968aa
Sean Ye <seanye24@gmail.com>
2022-11-07 01:14:58 -0800

    Add mdx

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,11 @@
+import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react';
-import visualizer from 'rollup-plugin-visualizer';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr(), visualizer()],
+  plugins: [react(), svgr(), mdx(), visualizer()],
   server: {
     port: 3000,
     proxy: {
