05e1bf7769108851aede6dbf9c877b9812aff06a
Sean Ye <seanye24@gmail.com>
2023-03-02 15:41:12 -0800

    Refactor updateDB script

--- packages/server/src/updateDB.ts
+++ packages/server/src/updateDB.ts
@@ -16,35 +16,71 @@ const quotes: RegexReplace = [/["'\u2018\u2019\u201C\u201D]/g, ''];
 const shrinkSpace: RegexReplace = [/\s\s+/g, ' '];
 const words: RegexReplace = [/note to moderator.*|bonuses:.*/g, ''];
 
+const normalizeAnswer = (answer: string) =>
+  nlp(
+    answer
+      .toLowerCase()
+      .replaceAll(...between)
+      .replaceAll(...quotes)
+      .replaceAll(...spacePunctuation)
+      .replaceAll(...uselessPunctuation)
+      .replaceAll(...usefulPunctuation)
+      .replaceAll(...words)
+      .replaceAll(...shrinkSpace)
+      .trim(),
+  )
+    .normalize()
+    .out('text')
+    .toLowerCase()
+    .replaceAll(...shrinkSpace)
+    .trim();
+
+const promiseAllWithProgress = (promises: Promise<any>[]) => {
+  let finished = 0;
+  promises.forEach((p) =>
+    p.then(() => {
+      finished += 1;
+      if (finished % 10000 === 0) {
+        console.log(`Finished ${finished} updates.`);
+      }
+      if (finished === promises.length) {
+        console.log('Finished all updates');
+      }
+    }),
+  );
+  return Promise.all(promises);
+};
+
 const client = new Client();
 (async () => {
   await client.connect();
-  console.log('querying from tossups.');
+  console.log(`Connected to database ${client.database}.`);
+
+  console.log('Adding a `normalized_answer` field to the `tossups` table.');
+
+  console.log('Retrieving all tossup answers.');
   const { rows } = await client.query<{ id: number; answer: string }>(
     'SELECT id,answer FROM tossups;',
   );
-  console.log('beginning to update answers.');
-  return Promise.all(
-    rows.map(async ({ id, answer }) => {
-      const normalizedAnswer = answer
-        .toLowerCase()
-        .replaceAll(...between)
-        .replaceAll(...quotes)
-        .replaceAll(...spacePunctuation)
-        .replaceAll(...uselessPunctuation)
-        .replaceAll(...usefulPunctuation)
-        .replaceAll(...words)
-        .replaceAll(...shrinkSpace)
-        .trim();
-      const postComp = nlp(normalizedAnswer)
-        .normalize()
-        .out('text')
-        .toLowerCase()
-        .replaceAll(...shrinkSpace)
-        .trim();
-      return client.query(
-        `UPDATE tossups SET normalized_answer='${postComp}' WHERE id=${id}`,
-      );
-    }),
-  );
-})().then(() => console.log('done updating answers..'));
+  console.log('Finished retrieving all tossup answers.');
+  console.log(`Found ${rows.length} tossup answers.`);
+
+  console.log('Inserting `normalized_answer` into `tossups`.');
+  const updates = rows.map(async ({ id, answer }, i) => {
+    if (i % 10000 === 0) {
+      console.log(`Queuing update for tossup ${i}`);
+    }
+    if (i === rows.length) {
+      console.log('Finished queueing updates.');
+    }
+
+    const normalizedAnswer = normalizeAnswer(answer);
+    return client.query(
+      `UPDATE tossups SET normalized_answer='${normalizedAnswer}' WHERE id=${id}`,
+    );
+  });
+  return promiseAllWithProgress(updates);
+})().then(() => {
+  console.log('Finished inserting `normalized_answer` into `tossups`.');
+  client.end().then(() => console.log('Postgres client disconnected.'));
+});
