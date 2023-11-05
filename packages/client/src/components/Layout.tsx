e5235ef052d156f2591bf75b53fd82be88f892ca
Sean Ye <seanye24@gmail.com>
2023-11-05 01:09:27 -0500

    Move modals to a Provider component

--- packages/client/src/components/Layout.tsx
+++ packages/client/src/components/Layout.tsx
@@ -1,13 +1,7 @@
 import { Center, Flex } from '@chakra-ui/react';
 import usePageHeight from '../hooks/usePageHeight';
-import SettingsModal from '../Settings';
-import BonusHistoryModal from './BonusHistoryModal';
 import Footer from './Footer';
-import HamburgerMenu from './HamburgerMenu';
 import Header from './Header';
-import InfoModal from './InfoModal';
-import TossupHistoryModal from './TossupHistoryModal';
-import UpdatesModal from './UpdatesModal';
 
 type Props = {
   children: React.ReactNode;
@@ -17,20 +11,12 @@ export default ({ children }: Props) => {
   const pageHeight = usePageHeight();
 
   return (
-    <>
-      <SettingsModal />
-      <UpdatesModal />
-      <InfoModal />
-      <HamburgerMenu />
-      <TossupHistoryModal />
-      <BonusHistoryModal />
-      <Flex direction="column" h={pageHeight}>
-        <Header />
-        <Center flexDir="column" overflow="auto" flex={1} px={3}>
-          {children}
-        </Center>
-        <Footer />
-      </Flex>
-    </>
+    <Flex direction="column" h={pageHeight}>
+      <Header />
+      <Center flexDir="column" overflow="auto" flex={1} px={3}>
+        {children}
+      </Center>
+      <Footer />
+    </Flex>
   );
 };
