34117dd8bc08fe5b646aee6db4a2cb0f50dac4fc
Sean Ye <seanye24@gmail.com>
2023-11-05 17:27:47 -0500

    Redesign buttons
    * make the icons smaller with a right margin
    * modify API slightly

--- packages/client/src/CluesGenerator/SearchButton.tsx
+++ packages/client/src/CluesGenerator/SearchButton.tsx
@@ -5,6 +5,10 @@ import {
 } from '../components/buttons';
 
 export default ({ to, ...rest }: Omit<RouterLinkIconButtonProps, 'icon'>) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <RouterLinkIconButton {...rest} to={to} icon={<SearchIcon w={6} h={6} />} />
+  <RouterLinkIconButton
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...rest}
+    to={to}
+    icon={<SearchIcon w={4} h={4} mr={1} />}
+  />
 );
