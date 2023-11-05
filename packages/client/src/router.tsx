c50ebda049c9baccf07405f0f74fa95c55a038fe
Sean Ye <seanye24@gmail.com>
2023-11-05 01:50:03 -0400

    Replace <BrowserRouter /> with createBrowserRouter()
    * use react router v6 patterns
    * update path names

---
+++ packages/client/src/router.tsx
@@ -0,0 +1,54 @@
+import { createBrowserRouter, Outlet } from 'react-router-dom';
+import About from './About';
+import BonusReader from './BonusReader';
+import Answers from './CluesGenerator/Answers';
+import Clues from './CluesGenerator/Clues';
+import Search from './CluesGenerator/Search';
+import Layout from './components/Layout';
+import FrequencyList from './FrequencyList';
+import TossupReader from './TossupReader';
+
+export default createBrowserRouter([
+  {
+    element: (
+      <Layout>
+        <Outlet />
+      </Layout>
+    ),
+    children: [
+      {
+        path: 'tossup',
+        element: <TossupReader />,
+      },
+      {
+        path: 'bonus',
+        element: <BonusReader />,
+      },
+      {
+        path: 'frequency',
+        element: <FrequencyList />,
+      },
+      {
+        path: 'clue',
+        children: [
+          {
+            index: true,
+            element: <Search />,
+          },
+          {
+            path: 'search/:answer',
+            element: <Answers />,
+          },
+          {
+            path: 'display/:answer',
+            element: <Clues />,
+          },
+        ],
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+]);
