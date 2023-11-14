17446f03d1f300a56cbc0aaa8fbbc84ef14a043f
Sean Ye <seanye24@gmail.com>
2023-11-14 01:02:18 -0500

    Add useSettings hook

--- packages/client/src/hooks/useSettings.ts
+++ packages/client/src/hooks/useSettings.ts
@@ -1,22 +1,52 @@
 import { useCallback, useMemo } from 'react';
+import { filterBonuses } from '../BonusReader/bonusReaderSlice';
+import { useAppDispatch } from '../redux/hooks';
+import { filterTossups } from '../TossupReader/tossupReaderSlice';
 import { useGlobalQueryParams } from '../utils/routes';
 import { saveSettings, Settings } from '../utils/settings';
 
-export const useSettings = () => {
+type SettingsHook = {
+  settings: Settings;
+  setSettings: (newSettings: Partial<Settings>) => void;
+};
+
+export const useSettings = (): SettingsHook => {
   const [params, setParams] = useGlobalQueryParams();
+  const dispatch = useAppDispatch();
+
+  const settings = useMemo<Settings>(
+    () => ({
+      categories: params.categories,
+      subcategories: params.subcategories,
+      difficulties: params.difficulties,
+      tournaments: params.tournaments,
+      fromYear: params.fromYear,
+      readingSpeed: params.readingSpeed,
+    }),
+    [
+      params.categories,
+      params.difficulties,
+      params.fromYear,
+      params.readingSpeed,
+      params.subcategories,
+      params.tournaments,
+    ],
+  );
 
   const setSettings = useCallback(
-    (newSettings: Partial<Settings>) => {
+    (settingsUpdate: Partial<Settings>) => {
+      const newSettings = { ...settings, ...settingsUpdate };
+      setParams(settingsUpdate);
+
+      // update questions
+      dispatch(filterTossups(newSettings));
+      dispatch(filterBonuses(newSettings));
+
       // save settings to local storage
       saveSettings(newSettings);
-
-      setParams(newSettings);
     },
-    [setParams],
+    [dispatch, setParams, settings],
   );
 
-  return useMemo(
-    () => ({ settings: params, setSettings }),
-    [params, setSettings],
-  );
+  return useMemo(() => ({ settings, setSettings }), [setSettings, settings]);
 };
