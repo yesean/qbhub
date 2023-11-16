b9ce7a8409a124c27d14ae1ea80d9e43e78e2169
Sean Ye <seanye24@gmail.com>
2023-11-16 03:02:00 -0500

    Add query param constraints for numbers

--- packages/client/src/utils/queryParams.ts
+++ packages/client/src/utils/queryParams.ts
@@ -1,4 +1,4 @@
-import { buildIsEnum } from '@qbhub/types';
+import { buildIsEnum, isNumber } from '@qbhub/types';
 import {
   decodeDelimitedNumericArray,
   encodeDelimitedNumericArray,
@@ -30,10 +30,57 @@ export const buildNumericEnumArrayParam = <T extends string, U extends number>(
   };
 };
 
-export const buildNeverNullParam = <T extends QueryParamConfig<U, V>, U, V>(
+const buildRangedNumberParam = <T extends typeof NumberParam>(
+  param: T,
+  minimumValue: number,
+  maximumValue: number,
+) => ({
+  encode(arg: Parameters<typeof param.encode>[0]) {
+    if (isNumber(arg) && (arg < minimumValue || arg > maximumValue)) {
+      return undefined;
+    }
+
+    return param.encode(arg);
+  },
+  decode(...args: Parameters<typeof param.decode>) {
+    const decodedValue = param.decode(...args);
+    if (decodedValue == null) return undefined;
+
+    if (decodedValue < minimumValue || decodedValue > maximumValue)
+      return undefined;
+
+    return decodedValue;
+  },
+});
+
+const buildSkipEncodeNumberParam = <T extends typeof NumberParam>(
+  param: T,
+  skipValue: number,
+) => ({
+  encode(arg: Parameters<typeof param.encode>[0]) {
+    if (isNumber(arg) && arg === skipValue) return undefined;
+
+    return param.encode(arg);
+  },
+  decode(...args: Parameters<typeof param.decode>) {
+    const decodedValue = param.decode(...args);
+
+    if (decodedValue == null) return undefined;
+    if (decodedValue === skipValue) return undefined;
+
+    return decodedValue;
+  },
+});
+
+const buildNeverNullParam = <T extends QueryParamConfig<U, V>, U, V>(
   param: T,
 ) => ({
-  ...param,
+  encode(...args: Parameters<typeof param.encode>) {
+    const encodedValue = param.encode(...args);
+    if (encodedValue == null) return undefined;
+
+    return encodedValue;
+  },
   decode(...args: Parameters<typeof param.decode>) {
     const decodedValue = param.decode(...args);
     if (decodedValue == null) return undefined;
@@ -41,6 +88,23 @@ export const buildNeverNullParam = <T extends QueryParamConfig<U, V>, U, V>(
     return decodedValue;
   },
 });
+
+export const buildNeverNullRangedNumberParamWithSkip = (
+  minimumValue: number,
+  maximumValue: number,
+  skipValue: number,
+) =>
+  buildNeverNullParam<
+    typeof NumberParam,
+    Parameters<typeof NumberParam.encode>[0],
+    ReturnType<typeof NumberParam.decode>
+  >(
+    buildRangedNumberParam(
+      buildSkipEncodeNumberParam(NumberParam, skipValue),
+      minimumValue,
+      maximumValue,
+    ),
+  );
 
 export const NeverNullNumberParam = buildNeverNullParam<
   typeof NumberParam,
