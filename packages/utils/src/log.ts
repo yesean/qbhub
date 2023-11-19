f3e30d3d0da79b52f29d6a24d6507e777b59e5c7
Sean Ye <seanye24@gmail.com>
2023-11-19 04:09:51 -0500

    Add log to util

---
+++ packages/utils/src/log.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+import { isLogLevelValid, LogLevel } from '@qbhub/types';
+import * as env from './env';
+
+const log = (logLevel: LogLevel, ...params: unknown[]) => {
+  // only log in development mode
+  if (!env.isDev) return;
+
+  // check if log level is valid, e.g. don't log debug messages in error level
+  if (!isLogLevelValid(logLevel, env.logLevel)) return;
+
+  if (logLevel === LogLevel.Debug) console.debug(...params);
+  if (logLevel === LogLevel.Info) console.info(...params);
+  if (logLevel === LogLevel.Warn) console.warn(...params);
+  if (logLevel === LogLevel.Error) console.error(...params);
+};
+
+const buildLog =
+  (logLevel: LogLevel) =>
+  (...params: unknown[]) =>
+    log(logLevel, ...params);
+
+export default {
+  debug: buildLog(LogLevel.Debug),
+  info: buildLog(LogLevel.Info),
+  warn: buildLog(LogLevel.Warn),
+  error: buildLog(LogLevel.Error),
+};
