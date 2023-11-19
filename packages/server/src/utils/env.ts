1a38ffcaf9cdb164d3ee362f1906ded18dcd7fdd
Sean Ye <seanye24@gmail.com>
2023-11-19 02:51:59 -0500

    Add log env

--- packages/server/src/utils/env.ts
+++ packages/server/src/utils/env.ts
@@ -1,2 +1,8 @@
+import { buildIsEnum, LogLevel } from '@qbhub/types';
+
 export const nodePort = process.env.NODE_PORT;
 export const isDev = process.env.NODE_ENV === 'development';
+
+const isLogLevel = buildIsEnum(LogLevel);
+const envLogLevel = process.env.LOGL_LEVEL;
+export const logLevel = isLogLevel(envLogLevel) ? envLogLevel : LogLevel.Warn;
