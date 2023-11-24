4dde53f72d9f0ec25e4e5910e2478ffc61ff92ea
Sean Ye <seanye24@gmail.com>
2023-11-24 16:49:11 -0500

    Add discord button in Footer

--- packages/client/src/components/Footer.tsx
+++ packages/client/src/components/Footer.tsx
@@ -1,15 +1,27 @@
 /// <reference types="vite-plugin-svgr/client" />
 import { InfoOutlineIcon, QuestionOutlineIcon } from '@chakra-ui/icons';
-import { Flex, Heading, IconButton, Link } from '@chakra-ui/react';
+import { Flex, Heading, Icon, IconButton, Link } from '@chakra-ui/react';
 import { useModalContext } from '../providers/ModalContext';
 import { ReactComponent as Donate } from '../static/coffee.svg';
+import { ReactComponent as Discord } from '../static/discord.svg';
 import { ReactComponent as Github } from '../static/github.svg';
+
+const discordURL = 'https://discord.gg/wWXfWUDdWm';
 
 const Footer: React.FC<React.PropsWithChildren<unknown>> = () => {
   const { openUpdatesModal, openInfoModal } = useModalContext();
 
   return (
     <Flex align="center" justify="center" p={3}>
+      <IconButton
+        aria-label="discord link"
+        bg="white"
+        icon={
+          <Link href={discordURL} isExternal w={5} h={5}>
+            <Icon as={Discord} w="110%" h="110%" color="#7289da" />
+          </Link>
+        }
+      />
       <IconButton
         aria-label="buymeacoffee link"
         bg="white"
