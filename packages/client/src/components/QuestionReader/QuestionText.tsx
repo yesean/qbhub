0446f79a932cd7a39f1918523a10116515a37ed3
Sean Ye <seanye24@gmail.com>
2023-11-26 09:48:54 -0500

    Simplify markup

--- packages/client/src/components/QuestionReader/QuestionText.tsx
+++ packages/client/src/components/QuestionReader/QuestionText.tsx
@@ -1,26 +1,32 @@
-import { Container } from '@chakra-ui/react';
-import { TossupWord } from '@qbhub/types';
+import { Box } from '@chakra-ui/react';
+import { isTossup } from '@qbhub/types';
+import { useMemo } from 'react';
+import { getTossupWords } from '../../utils/reader';
 import FormattedQuestion from '../reader/FormattedQuestion';
+import { useQuestionReaderContext } from './QuestionReaderContext';
+import useRevealer from './useRevealer';
 
-type Props = {
-  textWords: TossupWord[];
-  visibleIndex: number;
+export default () => {
+  const { question } = useQuestionReaderContext();
+
+  if (!isTossup(question)) return null;
+
+  const textWords = useMemo(
+    () => getTossupWords(question.formattedText),
+    [question.formattedText],
+  );
+
+  const { visibleIndex } = useRevealer({
+    words: textWords,
+    onFinish: () => {},
+  });
+
+  return (
+    <Box overflow="auto" bg="gray.100" p={4} borderRadius="md">
+      <FormattedQuestion
+        words={textWords}
+        indices={{ visible: visibleIndex }}
+      />
+    </Box>
+  );
 };
-
-export default ({ textWords, visibleIndex }: Props) => (
-  <Container
-    maxW="container.md"
-    overflow="auto"
-    bg="gray.100"
-    w="100%"
-    minH="100px"
-    mb={4}
-    p={4}
-    display="flex"
-    flexWrap="wrap"
-    justifyContent="start"
-    borderRadius="md"
-  >
-    <FormattedQuestion words={textWords} indices={{ visible: visibleIndex }} />
-  </Container>
-);
