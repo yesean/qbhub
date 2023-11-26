a947dfc1759589aa9383a14fbdc8b6b50d7d3dcc
Sean Ye <seanye24@gmail.com>
2023-11-26 14:32:31 -0500

    Conditionally show Answer

--- packages/client/src/components/QuestionReader/index.tsx
+++ packages/client/src/components/QuestionReader/index.tsx
@@ -1,8 +1,12 @@
 import { Flex } from '@chakra-ui/react';
-import { Tossup, isTossup } from '@qbhub/types';
+import { Tossup } from '@qbhub/types';
 import QuestionAnswer from './QuestionAnswer';
 import QuestionInfo from './QuestionInfo';
-import { QuestionReaderContextProvider } from './QuestionReaderContext';
+import {
+  QuestionReaderContextProvider,
+  QuestionReaderStatus,
+  useQuestionReaderContext,
+} from './QuestionReaderContext';
 import QuestionTextPlusInput from './QuestionTextPlusInput';
 
 type Props = {
@@ -14,10 +18,14 @@ type Props = {
 
 // eslint-disable-next-line arrow-body-style
 const QuestionReader = (_: Props) => {
+  const { status } = useQuestionReaderContext();
+
+  const shouldShowAnswer = status === QuestionReaderStatus.Judged;
+
   return (
     <Flex direction="column" maxW="container.md" overflow="auto" p={2} gap={4}>
       <QuestionInfo />
-      <QuestionAnswer />
+      {shouldShowAnswer && <QuestionAnswer />}
       <QuestionTextPlusInput />
     </Flex>
   );
@@ -25,8 +33,6 @@ const QuestionReader = (_: Props) => {
 
 export default (props: Props) => {
   const { question } = props;
-
-  if (!isTossup(question)) return null;
 
   return (
     // use `key` to reset question reader per question
