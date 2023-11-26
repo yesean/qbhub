e84390d6c126ef5bba3fe5cf8ee577455f08289f
Sean Ye <seanye24@gmail.com>
2023-11-26 12:49:36 -0500

    Merge question text + input

---
+++ packages/client/src/components/QuestionReader/QuestionTextPlusInput.tsx
@@ -0,0 +1,134 @@
+import { Box, Flex, Input } from '@chakra-ui/react';
+import { TossupResult } from '@qbhub/types';
+import { useCallback, useMemo, useRef, useState } from 'react';
+import useKeyboardShortcut from '../../hooks/useKeyboardShortcut';
+import {
+  Judge,
+  JudgeResult,
+  getTossupScore,
+  getTossupWords,
+} from '../../utils/reader';
+import TealButton from '../buttons/TealButton';
+import FormattedQuestion from '../reader/FormattedQuestion';
+import {
+  QuestionReaderStatus,
+  getNextStatus,
+  useQuestionReaderContext,
+} from './QuestionReaderContext';
+import useRevealer from './useRevealer';
+
+export const getInputBorderColor = (
+  status: QuestionReaderStatus,
+  questionResult: TossupResult | null,
+) => {
+  if (status !== QuestionReaderStatus.Judged || questionResult == null)
+    return 'gray.300';
+
+  if (questionResult.isCorrect) return 'green.400';
+  return 'red.400';
+};
+
+type Props = {};
+
+export default (_: Props) => {
+  const [userInput, setUserInput] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { question, status, setStatus, questionResult, setQuestionResult } =
+    useQuestionReaderContext();
+
+  const textWords = useMemo(
+    () => getTossupWords(question.formattedText),
+    [question.formattedText],
+  );
+
+  const { visibleIndex, pause, reveal } = useRevealer({
+    words: textWords,
+  });
+
+  const handleClick = useCallback(() => {
+    switch (status) {
+      case QuestionReaderStatus.Reading: {
+        if (inputRef.current != null) {
+          inputRef.current.disabled = false;
+          inputRef.current.focus();
+        }
+        pause();
+        break;
+      }
+      case QuestionReaderStatus.Answering: {
+        const judge = new Judge(question.formattedAnswer);
+        const isCorrect = judge.judge(userInput) === JudgeResult.correct;
+        const isInPower =
+          visibleIndex === -1 ? false : textWords[visibleIndex].bold;
+        const isBuzzAtEnd = visibleIndex === textWords.length - 1;
+        setQuestionResult({
+          buzzIndex: visibleIndex,
+          isCorrect,
+          userAnswer: userInput,
+          words: textWords,
+          tossup: question,
+          score: getTossupScore(isCorrect, isInPower, isBuzzAtEnd),
+        });
+
+        inputRef.current?.blur();
+        reveal();
+        break;
+      }
+      case QuestionReaderStatus.Judged: {
+        inputRef.current?.blur();
+        break;
+      }
+    }
+
+    setStatus(getNextStatus(status));
+  }, [
+    pause,
+    question,
+    reveal,
+    setQuestionResult,
+    setStatus,
+    status,
+    textWords,
+    userInput,
+    visibleIndex,
+  ]);
+
+  useKeyboardShortcut(' ', handleClick, {
+    customAllowCondition: status === QuestionReaderStatus.Reading,
+  });
+
+  useKeyboardShortcut('Enter', handleClick, {
+    allowHTMLInput: true,
+    customAllowCondition: status === QuestionReaderStatus.Answering,
+  });
+
+  useKeyboardShortcut('n', handleClick, {
+    customAllowCondition: status === QuestionReaderStatus.Judged,
+  });
+
+  const shouldShowBorder = status === QuestionReaderStatus.Judged;
+
+  return (
+    <>
+      <Box overflow="auto" bg="gray.100" p={4} borderRadius="md">
+        <FormattedQuestion
+          words={textWords}
+          indices={{ visible: visibleIndex, buzz: questionResult?.buzzIndex }}
+        />
+      </Box>
+      <Flex w="100%" justify="center">
+        <Input
+          ref={inputRef}
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          placeholder="Answer"
+          mr={4}
+          isDisabled={status !== QuestionReaderStatus.Answering}
+          borderColor={getInputBorderColor(status, questionResult)}
+          borderWidth={shouldShowBorder ? 2 : undefined}
+        />
+        <TealButton onClick={handleClick}>Buzz</TealButton>
+      </Flex>
+    </>
+  );
+};
--- packages/client/src/components/QuestionReader/useRevealer.ts
+++ packages/client/src/components/QuestionReader/useRevealer.ts
@@ -1,44 +1,40 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useSettings } from '../../hooks/useSettings';
 import { getReadingTimeoutDelay } from '../../utils/reader';
 import { DEFAULT_READING_SPEED } from '../../utils/settings/constants';
-import {
-  QuestionReaderStatus,
-  useQuestionReaderContext,
-} from './QuestionReaderContext';
 
 type Props = {
-  words: any[];
-  onFinish: () => void;
+  words: unknown[];
 };
 
-export default ({ words, onFinish }: Props) => {
+export default ({ words }: Props) => {
   const [visibleIndex, setVisibleIndex] = useState(-1);
+  const [isPaused, setIsPaused] = useState(false);
   const [isFinished, setIsFinished] = useState(false);
   const {
     settings: { readingSpeed },
   } = useSettings();
-  const { status } = useQuestionReaderContext();
 
   const readingDelay = getReadingTimeoutDelay(
     readingSpeed ?? DEFAULT_READING_SPEED,
   );
 
+  const pause = useCallback(() => setIsPaused(true), []);
+  const reveal = useCallback(
+    () => setVisibleIndex(words.length - 1),
+    [words.length],
+  );
+
   useEffect(() => {
+    // stop when there is no more words
     if (visibleIndex === words.length - 1) {
       if (!isFinished) {
         setIsFinished(true);
-        onFinish();
       }
       return;
     }
 
-    if (status === QuestionReaderStatus.Answering) return;
-
-    if (status === QuestionReaderStatus.Judged) {
-      setVisibleIndex(words.length - 1);
-      return;
-    }
+    if (isPaused) return;
 
     const timeoutID = setTimeout(() => {
       setVisibleIndex((prevIndex) => prevIndex + 1);
@@ -46,12 +42,14 @@ export default ({ words, onFinish }: Props) => {
 
     // eslint-disable-next-line consistent-return
     return () => clearTimeout(timeoutID);
-  }, [isFinished, onFinish, readingDelay, status, visibleIndex, words.length]);
+  }, [isFinished, isPaused, readingDelay, visibleIndex, words.length]);
 
   return useMemo(
     () => ({
       visibleIndex,
+      pause,
+      reveal,
     }),
-    [visibleIndex],
+    [pause, reveal, visibleIndex],
   );
 };
