dde943bfa9c531e073996162c4e0c14c1f248139
Sean Ye <seanye24@gmail.com>
2023-12-22 11:50:52 -0800

    Fix type issue

--- packages/client/src/components/buttons/RouterLinkButton.tsx
+++ packages/client/src/components/buttons/RouterLinkButton.tsx
@@ -1,7 +1,10 @@
+import { ComponentProps } from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import { LinkButton, Props as LinkButtonProps } from './LinkButton';
 
-const RouterLinkButton: React.FC<LinkButtonProps> = ({ to, ...rest }) => (
+const RouterLinkButton: React.FC<
+  ComponentProps<typeof ReactRouterLink> & LinkButtonProps
+> = ({ to, ...rest }) => (
   <LinkButton
     as={ReactRouterLink}
     to={to}
