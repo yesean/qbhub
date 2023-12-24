7eba77f2186846b9721ab81a26500c2f8403c8e9
Sean Ye <seanye24@gmail.com>
2023-12-24 12:00:16 -0800

    Add log level to output

--- packages/utils/src/log.ts
+++ packages/utils/src/log.ts
@@ -3,8 +3,11 @@ import { isLogLevelValid, LogLevel } from '@qbhub/types';
 import * as env from './env.js';
 
 const log = (logLevel: LogLevel, ...params: unknown[]) => {
+  const logLevelDisplay = `[${logLevel.toUpperCase()}]`;
+  const paramsWithLevel = [logLevelDisplay, ...params];
+
   // always log errors
-  if (logLevel === LogLevel.Error) console.error(...params);
+  if (logLevel === LogLevel.Error) console.error(...paramsWithLevel);
 
   // only log warn and below in development mode
   if (!env.isDev) return;
@@ -12,9 +15,9 @@ const log = (logLevel: LogLevel, ...params: unknown[]) => {
   // check if log level is valid, e.g. don't log debug messages in error level
   if (!isLogLevelValid(logLevel, env.logLevel)) return;
 
-  if (logLevel === LogLevel.Warn) console.warn(...params);
-  if (logLevel === LogLevel.Info) console.info(...params);
-  if (logLevel === LogLevel.Debug) console.debug(...params);
+  if (logLevel === LogLevel.Warn) console.warn(...paramsWithLevel);
+  if (logLevel === LogLevel.Info) console.info(...paramsWithLevel);
+  if (logLevel === LogLevel.Debug) console.debug(...paramsWithLevel);
 };
 
 export const debug = (...params: unknown[]) => log(LogLevel.Debug, ...params);
