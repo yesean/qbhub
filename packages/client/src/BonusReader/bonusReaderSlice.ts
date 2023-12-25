2798b44bd74b8240a0fc7fbcf892f3e918b185a7
Sean Ye <seanye24@gmail.com>
2023-12-25 14:41:47 -0800

    Simplify bonusReaderSlice

--- packages/client/src/BonusReader/bonusReaderSlice.ts
+++ packages/client/src/BonusReader/bonusReaderSlice.ts
@@ -1,53 +1,22 @@
-import {
-  Bonus,
-  BonusPart,
-  BonusPartResult,
-  BonusPartScore,
-  BonusResult,
-} from '@qbhub/types';
+import { Bonus, BonusResult } from '@qbhub/types';
 import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import type { RootState } from '../redux/store';
 import * as fetchUtils from '../utils/fetch';
-import { ReaderStatus, getBonusScore } from '../utils/reader';
 import { Settings } from '../utils/settings/types';
 import { isQuestionValid } from '../utils/settings/validate';
 
-type CurrentBonusState = {
-  bonus: Bonus;
-  buzzIndex: number;
-  number: number;
-  part: BonusPart;
-  partResult: BonusPartResult;
-  result: BonusResult;
-};
-
 type BonusReaderState = {
   bonuses: Bonus[];
-  current: CurrentBonusState;
   isFetching: boolean;
   results: BonusResult[];
   score: number;
-  status: ReaderStatus;
 };
 
 const initialState: BonusReaderState = {
   bonuses: [],
-  current: {
-    bonus: {} as Bonus,
-    buzzIndex: -1,
-    number: 1,
-    part: {} as BonusPart,
-    partResult: {} as BonusPartResult,
-    result: {
-      bonus: {} as Bonus,
-      parts: [],
-      score: 0,
-    },
-  },
   isFetching: false,
   results: [],
   score: 0,
-  status: ReaderStatus.idle,
 };
 
 const fetchBonuses = createAsyncThunk<
@@ -82,29 +51,10 @@ const bonusReaderSlice = createSlice({
     builder
       .addCase(nextBonus.pending, (state) => {
         state.isFetching = true;
-        state.status = ReaderStatus.fetching;
       })
       .addCase(nextBonus.fulfilled, (state) => {
-        if (state.bonuses.length === 0) {
-          state.status = ReaderStatus.empty;
-          return;
-        }
-
         state.isFetching = false;
-
-        // reset current
-        state.current = {
-          ...initialState.current,
-          result: { ...initialState.current.result },
-        };
-
-        // initialize current
-        const currentBonus = state.bonuses.shift() as Bonus;
-        state.current.bonus = currentBonus;
-        state.current.result.bonus = state.current.bonus;
-        state.current.part = state.current.bonus.parts[0] as BonusPart;
-
-        state.status = ReaderStatus.reading;
+        state.bonuses.shift();
       })
       .addCase(nextBonus.rejected, (state) => {
         state.isFetching = false;
@@ -113,83 +63,17 @@ const bonusReaderSlice = createSlice({
   initialState,
   name: 'bonusReader',
   reducers: {
-    buzz: (state, action: PayloadAction<number>) => {
-      if (state.status === ReaderStatus.reading) {
-        state.status = ReaderStatus.answering;
-        state.current.buzzIndex = action.payload;
-      }
-    },
     filterBonuses: (state, { payload: settings }: PayloadAction<Settings>) => {
       state.bonuses = state.bonuses.filter((bn) =>
         isQuestionValid(bn, settings),
       );
-    },
-    nextBonusPart: (state) => {
-      state.current.buzzIndex = initialState.current.buzzIndex;
-      state.current.partResult = initialState.current.partResult;
-      state.current.number += 1;
-      state.current.part = state.current.bonus.parts[
-        state.current.number - 1
-      ] as BonusPart;
-      state.status = ReaderStatus.reading;
-    },
-    prompt: (state) => {
-      if (
-        [ReaderStatus.answering, ReaderStatus.prompting].includes(state.status)
-      ) {
-        state.status = ReaderStatus.prompting;
-      }
-    },
-    submitAnswer: (
-      state,
-      action: PayloadAction<{
-        isCorrect: boolean;
-        userAnswer: string;
-      }>,
-    ) => {
-      if (
-        state.status === ReaderStatus.answering ||
-        state.status === ReaderStatus.prompting
-      ) {
-        state.status = ReaderStatus.partialJudged;
-
-        const bonusPart = state.current.bonus.parts[
-          state.current.number
-        ] as BonusPart;
-        const partResult = {
-          ...action.payload,
-          bonusPart,
-          buzzIndex: state.current.buzzIndex,
-          number: state.current.number,
-          score: action.payload.isCorrect
-            ? BonusPartScore.ten
-            : BonusPartScore.zero,
-        };
-        state.current.partResult = partResult;
-        state.current.result.parts.push(partResult);
-
-        if (state.current.number === 3) {
-          const score = getBonusScore(state.current.result.parts);
-          state.current.result.score = score;
-          state.score += score;
-          state.results.unshift(state.current.result);
-          state.status = ReaderStatus.judged;
-        }
-      }
     },
     submitResult(state, action: PayloadAction<BonusResult>) {
       state.results.push(action.payload);
     },
   },
 });
-export const {
-  buzz,
-  filterBonuses,
-  nextBonusPart,
-  prompt,
-  submitAnswer,
-  submitResult,
-} = bonusReaderSlice.actions;
+export const { filterBonuses, submitResult } = bonusReaderSlice.actions;
 
 export const selectBonusReader = ({ bonusReader }: RootState) => {
   const score = bonusReader.results.reduce(
@@ -201,9 +85,5 @@ export const selectBonusReader = ({ bonusReader }: RootState) => {
 
   return { ...bonusReader, currentBonus, score };
 };
-export const selectIsAnswering = (state: RootState) =>
-  [ReaderStatus.answering, ReaderStatus.prompting].includes(
-    state.bonusReader.status,
-  );
 
 export default bonusReaderSlice.reducer;
