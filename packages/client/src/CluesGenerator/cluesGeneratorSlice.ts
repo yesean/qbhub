1dbea3cd2ac3650edec0fedad7607e3a4b73eef6
Sean Ye <seanye24@gmail.com>
2023-12-25 22:25:51 -0800

    Simplify clueGeneratorSlice

--- packages/client/src/CluesGenerator/cluesGeneratorSlice.ts
+++ packages/client/src/CluesGenerator/cluesGeneratorSlice.ts
@@ -1,109 +1,74 @@
 import { FrequencyListEntry, SelectedClue } from '@qbhub/types';
-import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import type { RootState } from '../redux/store';
 import * as fetchUtils from '../utils/fetch';
 import { Settings } from '../utils/settings/types';
 
 const FETCH_LIMIT = 200;
 
-export enum CluesGeneratorStatus {
-  initial,
-  loading,
-  loaded,
-}
-
 type CluesGeneratorSlice = {
-  answers: FrequencyListEntry[];
-  clues: SelectedClue[];
-  currentQuery: string;
-  selectedAnswer: string;
-  status: CluesGeneratorStatus;
+  answers: FrequencyListEntry[] | undefined;
+  clues: SelectedClue[] | undefined;
+  isFetching: boolean;
 };
 
 const initialState: CluesGeneratorSlice = {
-  answers: [],
-  clues: [],
-  currentQuery: '',
-  selectedAnswer: '',
-  status: CluesGeneratorStatus.initial,
+  answers: undefined,
+  clues: undefined,
+  isFetching: false,
 };
 
 export const fetchAnswers = createAsyncThunk<
   FrequencyListEntry[],
   { answer: string; settings: Settings },
   { state: RootState }
->(
-  'cluesGenerator/fetchAnswers',
-  async (args) => {
-    const fetchParams = { ...args, limit: FETCH_LIMIT };
-    const answers = await fetchUtils.fetchAnswers(fetchParams);
-    return answers;
-  },
-  {
-    condition: (_, { getState }) => {
-      const {
-        cluesGenerator: { status },
-      } = getState();
-      return status !== CluesGeneratorStatus.loading;
-    },
-  },
-);
+>('cluesGenerator/fetchAnswers', async ({ answer, settings }) => {
+  const fetchParams = { ...settings, answer, limit: FETCH_LIMIT };
+  const answers = await fetchUtils.fetchAnswers(fetchParams);
+  return answers;
+});
+
 export const fetchClues = createAsyncThunk<
   SelectedClue[],
   { answer: string; settings: Settings },
   { state: RootState }
->(
-  'cluesGenerator/fetchClues',
-  async (args) => {
-    const fetchParams = { ...args, limit: FETCH_LIMIT };
-    const clues = await fetchUtils.fetchClues(fetchParams);
-    return clues;
-  },
-  {
-    condition: (_, { getState }) => {
-      const {
-        cluesGenerator: { status },
-      } = getState();
-      return status !== CluesGeneratorStatus.loading;
-    },
-  },
-);
+>('cluesGenerator/fetchClues', async ({ answer, settings }) => {
+  const fetchParams = { ...settings, answer, limit: FETCH_LIMIT };
+  const clues = await fetchUtils.fetchClues(fetchParams);
+  return clues;
+});
 
 const cluesGeneratorSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchAnswers.pending, (state) => {
-        state.status = CluesGeneratorStatus.loading;
+        state.isFetching = true;
       })
       .addCase(fetchAnswers.fulfilled, (state, action) => {
-        state.status = CluesGeneratorStatus.loaded;
+        state.isFetching = false;
         state.answers = action.payload;
+      })
+      .addCase(fetchAnswers.rejected, (state) => {
+        state.isFetching = false;
       });
+
     builder
       .addCase(fetchClues.pending, (state) => {
-        state.status = CluesGeneratorStatus.loading;
+        state.isFetching = true;
       })
       .addCase(fetchClues.fulfilled, (state, action) => {
-        state.status = CluesGeneratorStatus.loaded;
+        state.isFetching = false;
         state.clues = action.payload;
+      })
+      .addCase(fetchClues.rejected, (state) => {
+        state.isFetching = false;
       });
   },
   initialState,
   name: 'cluesGenerator',
-  reducers: {
-    resetStatus: (state) => {
-      state.status = CluesGeneratorStatus.initial;
-    },
-    selectAnswer: (state, action: PayloadAction<string>) => {
-      state.selectedAnswer = action.payload;
-    },
-    setQuery: (state, action: PayloadAction<string>) => {
-      state.currentQuery = action.payload;
-    },
-  },
+  reducers: {},
 });
-export const { resetStatus, selectAnswer, setQuery } =
-  cluesGeneratorSlice.actions;
+
 export const selectCluesGenerator = (state: RootState) => state.cluesGenerator;
 
 export default cluesGeneratorSlice.reducer;
