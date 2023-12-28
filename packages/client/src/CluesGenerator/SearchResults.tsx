fbe9bc029d6d70a87d62d29785cf2252708f9d60
Sean Ye <seanye24@gmail.com>
2023-12-28 02:02:22 -0800

    Refactor data fetching logic into wrapper component

--- packages/client/src/CluesGenerator/SearchResults.tsx
+++ packages/client/src/CluesGenerator/SearchResults.tsx
@@ -1,4 +1,5 @@
 import { Flex } from '@chakra-ui/react';
+import { FrequencyListEntry } from '@qbhub/types';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RouterLink } from '../components/RouterLink';
@@ -17,21 +18,15 @@ const answersFields = [
   { dataKey: 'frequency', label: 'Frequency' },
 ] as const;
 
-export default function SearchResults() {
-  const { answers, isFetching } = useSelector(selectCluesGenerator);
+type SearchResultsDisplayProps = {
+  answers: FrequencyListEntry[];
+};
+
+function SearchResultsDisplay({ answers }: SearchResultsDisplayProps) {
   const { getURL: getClueDisplayURL } = useClueDisplayRouteContext();
   const {
     params: { query },
   } = useClueSearchRouteContext();
-  const { dispatchFetchAnswers } = useActions();
-
-  useEffect(() => {
-    dispatchFetchAnswers(query);
-  }, [dispatchFetchAnswers, query]);
-
-  if (answers === undefined || isFetching) {
-    return <TableSkeleton />;
-  }
 
   if (answers.length === 0) {
     return <EmptySearchResults query={query} />;
@@ -61,4 +56,23 @@ export default function SearchResults() {
       />
     </Flex>
   );
+}
+
+export default function SearchResults() {
+  const { answers, isFetching } = useSelector(selectCluesGenerator);
+  const {
+    params: { query },
+  } = useClueSearchRouteContext();
+  const { dispatchFetchAnswers } = useActions();
+
+  useEffect(() => {
+    const promise = dispatchFetchAnswers(query);
+    return () => promise.abort();
+  }, [dispatchFetchAnswers, query]);
+
+  if (answers === undefined || isFetching) {
+    return <TableSkeleton />;
+  }
+
+  return <SearchResultsDisplay answers={answers} />;
 }
