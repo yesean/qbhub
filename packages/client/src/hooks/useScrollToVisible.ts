17a0f21a2722708494b01282bf817f7869ec8988
Sean Ye <seanye24@gmail.com>
2023-12-29 12:37:07 -0800

    Remove seamless-scroll-polyfill

--- packages/client/src/hooks/useScrollToVisible.ts
+++ packages/client/src/hooks/useScrollToVisible.ts
@@ -1,13 +1,11 @@
 import { useCallback, useMemo, useRef } from 'react';
-import { elementScrollIntoView } from 'seamless-scroll-polyfill';
 
 // Hook for scrolling an element into view
 export default function useScrollIntoView<T extends Element>() {
   const visibleRef = useRef<T>(null);
 
   const scrollToVisible = useCallback(() => {
-    if (visibleRef.current != null)
-      elementScrollIntoView(visibleRef.current, { block: 'center' });
+    if (visibleRef.current != null) visibleRef.current?.scrollIntoView(false);
   }, []);
 
   return useMemo(
