dd21f84434215880e90bd23877002ef439529e22
Sean Ye <seanye24@gmail.com>
2023-12-30 14:05:31 -0800

    Make limit an optional param

--- packages/server/src/utils/constants.ts
+++ packages/server/src/utils/constants.ts
@@ -63,5 +63,6 @@ export const TABLES = {
 export const MIN_TOURNAMENT_YEAR = 2005;
 export const MAX_TOURNAMENT_YEAR = 2020;
 
+export const DEFAULT_LIMIT = 10;
 export const MIN_LIMIT = 0;
 export const MAX_LIMIT = 200;
