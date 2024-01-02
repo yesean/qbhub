46d9444737ba3a397582e6ca42281d185c7a0abb
Sean Ye <seanye24@gmail.com>
2024-01-02 00:53:07 -0800

    Cleanup

--- packages/client/src/utils/questionReader.ts
+++ packages/client/src/utils/questionReader.ts
@@ -1,5 +1,3 @@
-import { Question } from '@qbhub/types';
-
 // question reader states
 export enum QuestionReaderStatus {
   Reading,
@@ -7,15 +5,11 @@ export enum QuestionReaderStatus {
   Judged,
 }
 
-type NextStatusOptions = {
-  isPrompted?: boolean;
-};
-
 // question reader state machine
-export const getNextStatus = (
+export function getNextStatus(
   prevStatus: QuestionReaderStatus,
-  { isPrompted = false }: NextStatusOptions = {},
-): QuestionReaderStatus => {
+  { isPrompted = false } = {},
+): QuestionReaderStatus {
   switch (prevStatus) {
     case QuestionReaderStatus.Reading:
       return QuestionReaderStatus.Answering;
@@ -26,14 +20,7 @@ export const getNextStatus = (
     case QuestionReaderStatus.Judged:
       return QuestionReaderStatus.Reading;
   }
-};
-
-export type UnscoredQuestionResult = {
-  buzzIndex: number;
-  isCorrect: boolean;
-  question: Question;
-  userAnswer: string;
-};
+}
 
 const SLOWEST_DELAY = 500;
 const FASTEST_DELAY = 25;
@@ -42,9 +29,9 @@ const DELAY_RANGE = SLOWEST_DELAY - FASTEST_DELAY;
 /**
  * Convert speed from percentage into a timeout delay
  */
-export const getReadingTimeoutDelay = (speed: number) => {
+export function getReadingTimeoutDelay(speed: number) {
   const scaledSpeed = 10 * Math.sqrt(speed); // skew speed towards faster end, common reading speed will probably in the faster end
   // speed is a number between 0 - 100, however, a higher speed means a lower delay
   // we also don't want delay to be 0, which is too fast
   return SLOWEST_DELAY - DELAY_RANGE * (scaledSpeed / 100);
-};
+}
