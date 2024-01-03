4fa988b6423a9bf886cef7245e387c92fc2eaec6
Sean Ye <seanye24@gmail.com>
2024-01-02 18:53:57 -0800

    Refactor tossup reader

---
+++ packages/client/src/TossupReader/EmptyTossupsNotice.tsx
@@ -0,0 +1,20 @@
+import { RepeatIcon } from '@chakra-ui/icons';
+import { Container, Flex, IconButton, Text } from '@chakra-ui/react';
+import useActions from '../hooks/useActions';
+
+export default function EmptyTossupsNotice() {
+  const { dispatchNextTossup } = useActions();
+
+  return (
+    <Flex align="center" direction="column" gap={4}>
+      <Container bg="gray.100" borderRadius="md" p={4}>
+        <Text>No tossups found, try adjusting your question settings.</Text>
+      </Container>
+      <IconButton
+        aria-label="Refetch tossups"
+        icon={<RepeatIcon />}
+        onClick={dispatchNextTossup}
+      />
+    </Flex>
+  );
+}
