dbf0a3fd412978e1480862d8f2f11dc238cb7a7b
Sean Ye <seanye24@gmail.com>
2024-01-04 16:32:12 -0800

    Add initial tossup results redesign

--- packages/client/src/TossupHistoryModal/TossupResults.tsx
+++ packages/client/src/TossupHistoryModal/TossupResults.tsx
@@ -1,84 +1,23 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { TossupResult } from '@qbhub/types';
-import FormattedQuestion from '../components/FormattedQuestion';
-import { VirtualizedTable, VirtualizedTableColumn } from '../components/tables';
-import { TOURNAMENT_METADATA_BY_TOURNAMENT } from '../utils/constants';
-import { parseHTMLString } from '../utils/reader';
+import TossupResultAccordion from './TossupResultAccordion';
 
 type TossupResultProps = {
   results: TossupResult[];
 };
 
-const cells: { [key: string]: (result: TossupResult) => JSX.Element } = {
-  answer: ({ tossup: { formattedAnswer } }) => (
-    <Box overflowWrap="break-word" textAlign="center">
-      {parseHTMLString(formattedAnswer)}
-    </Box>
-  ),
-  input: ({ userAnswer }) => (
-    <Box overflowWrap="break-word" textAlign="center">
-      {userAnswer || '<no answer>'}
-    </Box>
-  ),
-  question: ({ buzzIndex, formattedWords }) => (
-    <Box>
-      <FormattedQuestion buzzIndex={buzzIndex} words={formattedWords} />
-    </Box>
-  ),
-  score: ({ score }) => <Text align="center">{score}</Text>,
-  tournament: ({ tossup: { tournament } }) => (
-    <Box>{TOURNAMENT_METADATA_BY_TOURNAMENT[tournament].label}</Box>
-  ),
-};
-
 const TossupResults: React.FC<React.PropsWithChildren<TossupResultProps>> = ({
   results,
-}) => {
-  const columns: VirtualizedTableColumn<TossupResult>[] = [
-    {
-      cell: cells.score,
-      label: 'Score',
-      minWidth: 50,
-      proportion: 1,
-      useForHeight: false,
-    },
-    {
-      cell: cells.input,
-      label: 'Input',
-      minWidth: 120,
-      proportion: 1,
-      useForHeight: false,
-    },
-    {
-      cell: cells.answer,
-      label: 'Answer',
-      minWidth: 120,
-      proportion: 2,
-      useForHeight: true,
-    },
-    {
-      cell: cells.question,
-      label: 'Question',
-      minWidth: 150,
-      proportion: 4,
-      useForHeight: true,
-    },
-    {
-      cell: cells.tournament,
-      label: 'Tournament',
-      minWidth: 105,
-      proportion: 3,
-      useForHeight: false,
-    },
-  ];
-
-  return (
-    <VirtualizedTable
-      columns={columns}
-      results={results}
-      rowColor={(result) => (result.isCorrect ? 'green.200' : 'red.200')}
-    />
-  );
-};
+}) => (
+  <Flex direction="column" gap={4}>
+    {results.map((result, index) => (
+      <TossupResultAccordion
+        // eslint-disable-next-line react/no-array-index-key
+        key={`${result.tossup.id}${index}`}
+        result={result}
+      />
+    ))}
+  </Flex>
+);
 
 export default TossupResults;
