e6da4b3a33705127710c5071439dca7de75c5e2f
Sean Ye <seanye24@gmail.com>
2024-01-04 22:23:47 -0800

    Redesign tossup results summary

--- packages/client/src/TossupHistoryModal/index.tsx
+++ packages/client/src/TossupHistoryModal/index.tsx
@@ -1,25 +1,17 @@
-/* eslint-disable react/no-array-index-key */
 import {
+  Box,
   Modal,
   ModalBody,
   ModalContent,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
 } from '@chakra-ui/react';
-import { TossupScore } from '@qbhub/types';
 import { useSelector } from 'react-redux';
 import { selectTossupReader } from '../TossupReader/tossupReaderSlice';
 import TealButton from '../components/buttons/TealButton';
+import TossupHistorySummary from './TossupHistorySummary';
 import TossupResults from './TossupResults';
-
-const scoreHeaders = ['15', '10', '-5', 'Points'];
 
 type TossupHistoryModalProps = {
   closeModal: () => void;
@@ -30,13 +22,6 @@ const TossupHistoryModal: React.FC<
   React.PropsWithChildren<TossupHistoryModalProps>
 > = ({ closeModal, isOpen }) => {
   const { results } = useSelector(selectTossupReader);
-
-  const powers = results.filter((r) => r.score === TossupScore.power).length;
-  const tens = results.filter((r) => r.score === TossupScore.ten).length;
-  const negs = results.filter((r) => r.score === TossupScore.neg).length;
-  const points = results.reduce((acc, r) => acc + r.score, 0);
-
-  const scoreBody = [powers, tens, negs, points];
 
   return (
     <Modal
@@ -49,29 +34,14 @@ const TossupHistoryModal: React.FC<
       <ModalOverlay />
       <ModalContent h="max(75vh, 600px)" m={4} w="min(90vw, 1400px)">
         <ModalHeader>Tossup History</ModalHeader>
-        <ModalBody display="flex" flexDirection="column" pt={0}>
-          <Table mb={4} variant="simple">
-            <Thead>
-              <Tr>
-                {scoreHeaders.map((header, i) => (
-                  <Th key={i}>{header}</Th>
-                ))}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                {scoreBody.map((score, i) => (
-                  <Td key={i}>{score}</Td>
-                ))}
-              </Tr>
-            </Tbody>
-          </Table>
+        <ModalBody display="flex" flexDirection="column" gap={4}>
+          <Box flexShrink={0}>
+            <TossupHistorySummary />
+          </Box>
           <TossupResults results={results} />
         </ModalBody>
         <ModalFooter>
-          <TealButton mr={3} onClick={closeModal}>
-            Done
-          </TealButton>
+          <TealButton onClick={closeModal}>Done</TealButton>
         </ModalFooter>
       </ModalContent>
     </Modal>
