27a7858e47ad63e3f8f8d161d604530990c4d0f9
Sean Ye <seanye24@gmail.com>
2024-01-07 11:45:03 -0800

    Build a declarative table api

---
+++ packages/client/src/components/Table.tsx
@@ -0,0 +1,47 @@
+import {
+  Table as ChakraTable,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+import { useMemo } from 'react';
+import useTable, { UseTableProps } from '../hooks/useTable';
+
+type TableProps<T> = React.ComponentProps<typeof TableContainer> &
+  UseTableProps<T>;
+
+export function Table<T>({
+  columns,
+  data,
+  ...tableContainerProps
+}: TableProps<T>) {
+  const useTableProps = useMemo(() => ({ columns, data }), [columns, data]);
+  const { headers, rows } = useTable<T>(useTableProps);
+
+  return (
+    <TableContainer {...tableContainerProps}>
+      <ChakraTable layout="fixed">
+        <Thead bgColor="white" pos="sticky" top={0}>
+          <Tr>
+            {headers.map((header) => (
+              <Th key={header.id}>{header.element}</Th>
+            ))}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {rows.map((cells, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <Tr key={index}>
+              {cells.map((cell) => (
+                <Td key={cell.columnID}>{cell.element}</Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      </ChakraTable>
+    </TableContainer>
+  );
+}
