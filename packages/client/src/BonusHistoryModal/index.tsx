cb78bea45b4e28e2911637c2527837940b8ab0a1
Sean Ye <seanye24@gmail.com>
2024-02-17 18:20:25 -0500

    Refactor question history modal layout into component

--- packages/client/src/BonusHistoryModal/index.tsx
+++ packages/client/src/BonusHistoryModal/index.tsx
@@ -1,15 +1,4 @@
-/* eslint-disable react/no-array-index-key */
-import {
-  Box,
-  Flex,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-} from '@chakra-ui/react';
-import TealButton from '../components/buttons/TealButton';
+import QuestionHistoryModal from '../components/QuestionHistoryModal/QuestionHistoryModal';
 import BonusHistoryBody from './BonusHistoryBody';
 import BonusHistorySummary from './BonusHistorySummary';
 
@@ -18,35 +7,12 @@ type BonusHistoryModalProps = {
   isOpen: boolean;
 };
 
-export default function BonusHistoryModal({
-  closeModal,
-  isOpen,
-}: BonusHistoryModalProps) {
+export default function BonusHistoryModal(props: BonusHistoryModalProps) {
   return (
-    <Modal
-      isCentered
-      isOpen={isOpen}
-      onClose={closeModal}
-      scrollBehavior="inside"
-      size="6xl"
-    >
-      <ModalOverlay />
-      <ModalContent h="max(75vh, 600px)" m={4} w="min(90vw, 1400px)">
-        <ModalHeader>Bonus History</ModalHeader>
-        <ModalBody>
-          <Flex direction="column" gap={4} h="100%">
-            <Box flexShrink={0}>
-              <BonusHistorySummary />
-            </Box>
-            <BonusHistoryBody />
-          </Flex>
-        </ModalBody>
-        <ModalFooter>
-          <TealButton mr={3} onClick={closeModal}>
-            Done
-          </TealButton>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+    <QuestionHistoryModal
+      {...props}
+      body={<BonusHistoryBody />}
+      summaryHeader={<BonusHistorySummary />}
+    />
   );
 }
